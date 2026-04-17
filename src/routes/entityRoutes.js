e486f1eace63e6343470b9ac804a8ba9f67a30af
Hakeem Fujah <53516539+Hacka-more@users.noreply.github.com>
2026-04-17 12:42:24 +0100

    feat: add identity verification columns to users table and implement trust-based entity suggestion endpoint

--- src/routes/entityRoutes.js
+++ src/routes/entityRoutes.js
@@ -4,6 +4,7 @@ const entityController = require('../controllers/entityController');
 const { verifyToken, optionalVerify } = require('../middlewares/authMiddleware');
 
 router.get('/search', optionalVerify, entityController.searchEntities);
+router.get('/suggestions', verifyToken, entityController.getSuggestions);
 router.post('/register', verifyToken, entityController.createEntity);
 
 module.exports = router;
